f728ac9315bef9f921237591781d93076105e5c6
Juan García <106031776+JuanGarciaUTPL@users.noreply.github.com>
2024-01-30 20:56:32 -0500

    primer ejercicio

---
+++ proyecto_paisBarrio/mapa/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from mapas.models import Pais,Barrio
+
+admin.site.register(Pais)
+admin.site.register(Barrio)
---
+++ proyecto_paisBarrio/mapa/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# se importa las vistas de la aplicación
+from . import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+        path('', TemplateView.as_view(template_name='listadoOpciones.html'), name='listadoOpciones'),
+        path('listarPaises', views.listarPaises, name='listarPaises'),
+        path('listarBarrios', views.listarBarrios, name='listarBarrios'),
+ ]
